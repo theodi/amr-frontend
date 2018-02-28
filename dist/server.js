1b822840e7f98b10e93444b9e011e792fb28e914
Phil Lang <plangphoto@gmail.com>
2018-02-28 15:09:40 +0000

    Allow for history-api-fallback, so that a page refresh loads

--- dist/server.js
+++ dist/server.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
+const express = require('express');
+const path = require('path');
+const history = require('connect-history-api-fallback')
+const serveStatic = require('serve-static');
+const app = express();
 
-app = express();
 app.use(serveStatic(__dirname));
+app.use(history())
+app.use(serveStatic(__dirname));
+
+app.get("/", function (req, res) {
+ res.render(path.join(__dirname + "/index.html"))
+})
 
 var port = process.env.PORT || 5000;
 app.listen(port);

c5cd3a0af92ba4f0f59bb672c1224431508f8bf4
Phil Lang <plangphoto@gmail.com>
2018-03-12 16:21:17 +0000

    Update about page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,42 @@
 <template>
 	<div>
-		<h2 class="title-head">Welcome</h2>
-		<p>The threat caused by antimicrobial resistance
-		(AMR) is a growing and urgent global health problem. Currently up to
-		700,000 people globally, including 50,000 people in Europe and the USA,
-		die each year due to drug resistance in illnesses such as bacterial
-		infections, malaria, HIV/AIDS and tuberculosis.</p>
+		<p>The threat caused by <a href="http://www.who.int/antimicrobial-resistance/en/">
+		antimicrobial resistance</a> (AMR) is a growing and
+		urgent global health problem. Currently, up to 700,000 people globally,
+		including 50,000 people in Europe and the USA, die each year due to drug
+		resistance in illnesses such as bacterial infections, malaria, HIV/AIDS and
+		tuberculosis.</p>
 		<p>In 2016, a number of pharmaceutical companies signed two declarations,
 		committing to sharing surveillance data to make it accessible to public
-		health bodies and healthcare professionals.</p>
+		health bodies and healthcare professionals:</p>
 		<ul>
-			<li>The Davos declaration (January 2016)</li>
-			<li>The United Nations Global Assembly Declaration (September 2016)</li>
+			<li>The <a href="https://amr-review.org/industry-declaration.html">
+			Davos declaration</a> (January 2016)</li>
+			<li>The <a href="https://www.un.org/pga/71/wp-content/uploads/sites/40/2016/09/DGACM_GAEAD_ESCAB-AMR-Draft-Political-Declaration-1616108E.pdf">
+			United Nations Global Assembly Declaration</a> (September 2016)</li>
 		</ul>
 		<p>There is an accepted need to share data form the global antibiotic
 		surveillance studies externally to guide appropriate antibiotic prescription,
 		to set up new breakpoints for antibiotics and to encourage wider innovation
 		in this area.</p>
+		<h2>Wellcome Trust</h2>
+		<p>The Wellcome Trust is a global charitable foundation, both politically and
+		financially independent. We support scientists and researchers, take on big
+		problems, fuel imaginations, and spark debate.</p>
+		<p>Find out more about the Wellcome Trust <a href="https://wellcome.ac.uk/about-us">here</a></p>
+		<h2>ODI</h2>
+		<p>The ODI was co-founded in 2012 by the inventor of the web Sir Tim Berners-Lee
+		and Artificial Intelligence expert Sir Nigel Shadbolt to advocate for the
+		innovative use of open data to affect positive change across the globe.</p>
+		<p>It is an independent, non-profit, non-partisan company that has welcomed
+		high profile board members including Lastminute.com founder Baroness Martha
+		Lane Fox and former European Commissioner Neelie Kroes.</p>
+		<p>Situated in London, with an international reach, hundreds of members,
+		thousands of people trained, dozens of startups incubated, and a convening
+		space based in the heart of London’s thriving Shoreditch area, the ODI invites
+		everyone interested in developing with data – whether on an individual,
+		organisational or global level – to get in touch.</p>
+		<p>Find out more about the ODI <a href="https://theodi.org">here</a></p>
 		<h2>About the initiative</h2>
 		<p>This initiative, grant-funded by the Wellcome Trust, will focus on openly
 			publishing industry sponsored human antimicrobial surveillance data in order:</p>
@@ -33,6 +53,20 @@
 		<p>Insights and learnings can then be derived which will benefit clinicians,
 			global organisations such as the United Nations, the World Health
 			Organisation, and wider society.</p>
+		<h2>Plan</h2>
+		<p>The initiative is structured around the following activities:</p>
+		<ul>
+			<li>Workshop of interested parties to develop a plan for common standards
+			and a single portal was held in July 2017 in London.</li>
+			<li>Three-month research phase that will began in November 2017 - which
+			aims to identify all available human antimicrobial surveillance data, to
+			assess how openly available these data sets are</li>
+			<li>Second workshop held in March 2018 to disseminate the results of the
+			research phase and to present the online portal. The outputs of this workshop
+			will determine the next stages of the initiative.</li>
+		</ul>
+		<h2>Contact</h2>
+		<p>For further information, please contact <a href="mailto:david.beardmore@theodi.org">David Beardmore</a>, Commercial Director at the Open Data Institute.</p>
 	</div>
 </template>
 
--- src/components/Methodology.vue
+++ src/components/Methodology.vue
@@ -1,31 +1,25 @@
 <template>
 	<div>
 		<h2 class="title-head">Methodology</h2>
-		<p>The goal of the first phase of this project was to establish a register
-		of information about AMR surveillance datasets held by pharmaceutical companies.</p>
-		<p>The scope of the register in terms of the information it could contain
-		was informed by the timescale of the project. Initial conversations with
-		representatives from pharmaceutical companies suggested that gathering the
-		kind of information we wanted on their surveillance datasets would not always
-		be a simple process.</p>
-		<p>We initially compiled a list of fields that it would be desirable to collect
-		information on and discussed surveillance methodology with experts from
-		partner pharmaceutical companies. We then drafted a questionnaire based on
-		these discussions and the project’s aims. After multiple rounds of feedback
+		<p>We initially compiled a list of fields that it would be desirable to
+		collect information on and discussed surveillance methodology with experts
+		from partner pharmaceutical companies. We then drafted a questionnaire based
+		on these discussions and the project’s aims. After multiple rounds of feedback
 		from experts on the project’s steering group, including those from partner
-		pharmaceutical companies, we finalised a short eight-page questionnaire.</p>
+		pharmaceutical companies, we finalised a short eight-page questionnaire.
+		Many were omitted for future consideration rather than risk overloading the
+		companies with requests for information.</p>
 		<p>We contacted representatives from twelve major pharmaceutical companies
-		explaining the project and asking them to fill out this questionnaire.
-		Initial responses were reviewed and clarified where necessary, in some cases
-		involving ongoing discussion. The register contains AMR surveillance programmes
-		from six companies, as not all companies had relevant activities to report.</p>
+		explaining the project and asking them to fill out this questionnaire. Initial
+		responses were reviewed and clarified where necessary, in some cases involving
+		ongoing discussion. The register currently contains AMR surveillance programmes
+		from six companies, as not all companies had relevant activities to report
+		at this point in time.</p>
 		<p>The responses were then cleaned and converted into JSON format. The data
 		structure of the register consists of three types of object: companies,
 		surveillance programmes, and antimicrobials. Companies can have multiple
-		programmes; programmes can collect data on multiple antimicrobials; and
-		antimicrobials can sit within multiple programmes and companies.</p>
-		<p>The register was built by the tech team at the Open Data Institute with
-		feedback from the project’s steering group.</p>
+		programmes; programmes can collect data on multiple antimicrobials; which
+		themselves can sit within multiple programmes and companies.</p>
 	</div>
 </template>
 

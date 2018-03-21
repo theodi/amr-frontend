095c9ef400c08dfde2ad00a929c49b747acde07d
Phil Lang <plangphoto@gmail.com>
2018-03-21 15:40:10 +0000

    Add contextual copy for "Programme" index page

--- src/components/Programmes.vue
+++ src/components/Programmes.vue
@@ -1,6 +1,14 @@
 <template>
 	<div>
 		<h2 class="title-head">Research programmes</h2>
+		<p>Pharmaceutical industry programmes are conducted to fulfil regulatory
+		requirements for antibiotics in development and also as post approval
+		commitments to monitor susceptibility and resistance for marketed antibiotics.</p>
+		<p>Programmes are usually global in scope and conducted via experienced
+		vendors within three years of expected filing and continued post approval to
+		monitor changes in resistance patterns and trends at a global, regional and
+		local level and to provide healthcare practitioners with susceptibility data
+		to support the appropriate use of antibiotics.</p>
 		<div v-for="p in sortedArray" :key="p.id">
 			<div class="box">
 				<a v-bind:href="/programmes/ + p.url">
